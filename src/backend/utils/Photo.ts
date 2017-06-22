6545126f4eec1ffad1ceac6ebccc6065459efdff
tkir <kirill.titenko@gmail.com>
2017-06-22 17:01:54 +0300

    picture api in process

---
+++ src/backend/utils/Photo.ts
@@ -0,0 +1,11 @@
+export class Photo {
+  public id: string;
+  public owner: string;
+  public secret: string;
+  public server: string;
+  public farm: string;
+  public title: string;
+  public ispublick: boolean;
+  public isfriend: boolean;
+  public isfamily: boolean;
+}
